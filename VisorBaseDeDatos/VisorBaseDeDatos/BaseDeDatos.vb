efee87bf192b784ec352c5414c4eb14498131893
Sebastián de los Ángeles <luissebastianangeles@gmail.com>
2019-06-11 15:08:44 -0300

    Nuevo còdigo

--- VisorBaseDeDatos/VisorBaseDeDatos/BaseDeDatos.vb
+++ VisorBaseDeDatos/VisorBaseDeDatos/BaseDeDatos.vb
@@ -1,12 +1,10 @@
 ﻿Imports MySql.Data.MySqlClient
-Imports System.Security.Cryptography
-Imports System.Text
-Imports System.IO
 
 Module BaseDeDatos
 
-    Function CrearConexion(servidor As String, usuario As String, contrasenia As String) _
-    As MySqlConnection
+    Function CrearConexion(servidor As String,
+                           usuario As String,
+                           contrasenia As String) As MySqlConnection
         Dim conexion As New MySqlConnection
         conexion.ConnectionString = "server=" & servidor & ";" &
                                     "user=" & usuario & ";" &
@@ -22,13 +20,13 @@ Module BaseDeDatos
         Try
             conexion.Open()
             MsgBox("Conectado al servidor")
-            Dim comando As MySqlCommand = New MySqlCommand(consulta, conexion)
+            Dim comando As MySqlCommand =
+                New MySqlCommand(consulta, conexion)
             Dim lector As MySqlDataReader = comando.ExecuteReader()
             If lector.HasRows Then
                 While lector.Read()
                     comboBox.Items.Add(lector.GetString(0))
                 End While
-
             Else
                 MsgBox("ATENCIÓN:No se obtuvo ningún dato del servidor.")
             End If
@@ -39,7 +37,9 @@ Module BaseDeDatos
         conexion.Close()
     End Sub
 
-    Sub VerTablas(conexion As MySqlConnection, base As String, comboBox As ComboBox)
+    Sub VerTablas(conexion As MySqlConnection,
+                  base As String,
+                  comboBox As ComboBox)
         Dim consulta As String = "SHOW TABLES FROM " + base
         comboBox.Items.Clear()
 
@@ -61,6 +61,34 @@ Module BaseDeDatos
         End Try
         conexion.Close()
         comboBox.SelectedIndex = 0
+    End Sub
+
+    Sub VerCampos(conexion As MySqlConnection,
+                  base As String,
+                  tabla As String,
+                  listBox As ListBox)
+        Dim consulta As String = "SHOW COLUMNS FROM " + base + "." + tabla
+        listBox.Items.Clear()
+
+        Try
+            conexion.Open()
+            Dim comando As MySqlCommand = New MySqlCommand(consulta, conexion)
+            Dim lector As MySqlDataReader = comando.ExecuteReader()
+            If lector.HasRows Then
+                While lector.Read()
+                    listBox.Items.Add(lector.GetString(0))
+                End While
+
+            Else
+                MsgBox("ATENCIÓN:No se obtuvo ningún dato del servidor.")
+            End If
+        Catch ex As Exception
+            MsgBox("No se ha podido conectar al servidor.")
+            MsgBox("Error: " & ex.ToString)
+        End Try
+        conexion.Close()
+        listBox.SelectedIndex = 0
+
     End Sub
 
 End Module
--- VisorBaseDeDatos/VisorBaseDeDatos/Form1.Designer.vb
+++ VisorBaseDeDatos/VisorBaseDeDatos/Form1.Designer.vb
@@ -39,119 +39,131 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 201)
+        Me.Button1.Location = New System.Drawing.Point(9, 163)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(202, 37)
+        Me.Button1.Size = New System.Drawing.Size(152, 30)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Conectar"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 27)
+        Me.TextBox1.Location = New System.Drawing.Point(9, 22)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(202, 22)
+        Me.TextBox1.Size = New System.Drawing.Size(152, 20)
         Me.TextBox1.TabIndex = 1
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(12, 85)
+        Me.TextBox2.Location = New System.Drawing.Point(9, 69)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(202, 22)
+        Me.TextBox2.Size = New System.Drawing.Size(152, 20)
         Me.TextBox2.TabIndex = 2
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(12, 147)
+        Me.TextBox3.Location = New System.Drawing.Point(9, 119)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(202, 22)
+        Me.TextBox3.Size = New System.Drawing.Size(152, 20)
         Me.TextBox3.TabIndex = 3
         Me.TextBox3.UseSystemPasswordChar = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 7)
+        Me.Label1.Location = New System.Drawing.Point(9, 6)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(61, 17)
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Servidor"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 65)
+        Me.Label2.Location = New System.Drawing.Point(9, 53)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(57, 17)
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "Usuario"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 127)
+        Me.Label3.Location = New System.Drawing.Point(9, 103)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(81, 17)
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
         Me.Label3.TabIndex = 6
         Me.Label3.Text = "Contraseña"
         '
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(284, 27)
+        Me.ComboBox1.Location = New System.Drawing.Point(213, 22)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(218, 24)
+        Me.ComboBox1.Size = New System.Drawing.Size(164, 21)
         Me.ComboBox1.TabIndex = 7
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(281, 7)
+        Me.Label4.Location = New System.Drawing.Point(211, 6)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(108, 17)
+        Me.Label4.Size = New System.Drawing.Size(82, 13)
         Me.Label4.TabIndex = 8
         Me.Label4.Text = "Bases de Datos"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(281, 65)
+        Me.Label5.Location = New System.Drawing.Point(211, 53)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(51, 17)
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
         Me.Label5.TabIndex = 10
         Me.Label5.Text = "Tablas"
         '
         'ComboBox2
         '
         Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(284, 85)
+        Me.ComboBox2.Location = New System.Drawing.Point(213, 69)
+        Me.ComboBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(218, 24)
+        Me.ComboBox2.Size = New System.Drawing.Size(164, 21)
         Me.ComboBox2.TabIndex = 9
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 16
-        Me.ListBox1.Location = New System.Drawing.Point(284, 146)
+        Me.ListBox1.Location = New System.Drawing.Point(213, 119)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(217, 180)
+        Me.ListBox1.Size = New System.Drawing.Size(164, 147)
         Me.ListBox1.TabIndex = 11
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(281, 126)
+        Me.Label6.Location = New System.Drawing.Point(211, 102)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(70, 17)
+        Me.Label6.Size = New System.Drawing.Size(53, 13)
         Me.Label6.TabIndex = 12
         Me.Label6.Text = "Columnas"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(538, 347)
+        Me.ClientSize = New System.Drawing.Size(404, 282)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Label5)
@@ -165,6 +177,7 @@ Partial Class Form1
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)

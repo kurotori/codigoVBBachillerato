1b9244ab84e4a92ba68c4bce4497770d730a5867
Sebastián de los Angeles <luissebastianangeles@gmail.com>
2019-08-27 13:01:08 -0300

    Avance de código

--- Chat/Chat/Form1.vb
+++ Chat/Chat/Form1.vb
@@ -1,23 +1,47 @@
 ﻿Public Class Form1
 
+    Dim usuario As String = "Yo"
+    Dim usuario2 As String = "Usuario2"
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Tiempo.Interval = 5000
         Tiempo.Start()
     End Sub
 
-    Private Sub Recibir_mensajes()
-        Dim usuario As String = vbNewLine & "Usuario2"
-        Dim mensaje As String = vbNewLine & "bla bla bla"
-
-        Dim marcaDeTiempo As String = vbNewLine & vbTab & "-- " &
+    Function MarcaDeTiempo() As String
+        Dim marca As String = vbNewLine & vbTab & "-- " &
             String.Format("{0:dd/MM/yyyy}", DateTime.Now) & " " &
             String.Format("{0:HH:mm:ss}", DateTime.Now) & " --"
 
-        Txtbx_mensajes.AppendText(usuario & ": " & mensaje & marcaDeTiempo)
+        Return marca
+    End Function
+
+    Private Sub Recibir_mensajes()
+        Dim mensaje As String = "bla bla bla"
+
+        Dim tiempo As String = MarcaDeTiempo()
+
+        Txtbx_mensajes.AppendText(vbNewLine & usuario2 & ": " & vbNewLine &
+                                  mensaje & vbNewLine & tiempo)
 
     End Sub
 
+    Private Sub Enviar_mensajes()
+        Dim mensaje As String = Txtbx_mensaje_enviar.Text
+        Txtbx_mensaje_enviar.Text = vbNullString
+
+        Dim tiempo As String = MarcaDeTiempo()
+
+        Txtbx_mensajes.AppendText(vbNewLine & usuario & ": " & vbNewLine &
+                                  mensaje & vbNewLine & tiempo)
+    End Sub
+
+
     Private Sub Tiempo_Tick(sender As Object, e As EventArgs) Handles Tiempo.Tick
         Recibir_mensajes()
+    End Sub
+
+    Private Sub Btn_enviar_Click(sender As Object, e As EventArgs) Handles Btn_enviar.Click
+        Enviar_mensajes()
     End Sub
 End Class

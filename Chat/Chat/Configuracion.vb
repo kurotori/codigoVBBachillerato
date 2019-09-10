82e1e5ccf815166ed16a982a9e9680f51b1e400d
Sebastián de los Ángeles <luissebastianangeles@gmail.com>
2019-09-09 21:35:39 -0300

    Actualización de còdigo

--- Chat/Chat/Configuracion.vb
+++ Chat/Chat/Configuracion.vb
@@ -1,7 +1,8 @@
 ﻿Public Class Configuracion
 
-
-
+    Dim servidor As String
+    Dim usuario As String
+    Dim constrasenia As String
 
     Private Sub btn_guardar_config_Click(sender As Object, e As EventArgs) Handles btn_guardar_config.Click
 
